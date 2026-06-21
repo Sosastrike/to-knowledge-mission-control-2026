5e4022df95d21cb1e564add973aed7862e4884d1
Tony <tony@srv1568353.hstgr.cloud>
2026-06-21 09:41:34 -0400

    test(auth): load jest-dom in Gateway logout test

--- src/components/gateway/GatewayShell.auth.test.tsx
+++ src/components/gateway/GatewayShell.auth.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '@testing-library/jest-dom/vitest'
 import { act, fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import GatewayShell from './GatewayShell'
